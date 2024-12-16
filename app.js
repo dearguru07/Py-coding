6251b52fd67c4ece71a03fb4a9c4568009fc602d
dearguru07 <pangalaguruyadav101@gmail.com>
2024-12-16 17:32:52 +0530

    js

--- app.js
+++ app.js
@@ -2,19 +2,41 @@
 // x=20
 // console.log(x)
 
+// const all=[1,5,'guru','charan','75',44,75,68]
+// const strs=[]
+// const values=[]
 
-const all=[1,5,'guru','charan','75',44,75,68]
-const strs=[]
-const values=[]
+// const res=all.forEach((x)=>{
+//     if(typeof(x)==='string'){
+//         strs.push(x)
+//     }
+//     else{
+//         values.push(x)
+//     }
 
-const res=all.forEach((x)=>{
-    if(typeof(x)==='string'){
-        strs.push(x)
-    }
-    else{
-        values.push(x)
-    }
+// })
+// console.log(strs)
+// console.log(values)
 
-})
-console.log(strs)
-console.log(values)
+// console.log()
+
+// console.log(a);
+// Name()
+// var a = 20;
+// function Name() {
+//     function n(){
+//         console.log(a)
+//     }
+//     console.log('Guru')
+// }
+
+
+
+var a=20
+console.log(a)
+
+function b(){
+    var a=20
+    console.log(a)
+}
+b()
--- index.py
+++ index.py
@@ -4,10 +4,12 @@
 # string=str(Nuumber)
 # length=len(string)
 
-m=int(input('Nenter a nymber'))
-string=str(m)
-# print(string)
-length=len(string)
-# print(length)
-last=string(len-1)
-print(last)
+# m=int(input('Nenter a nymber'))
+# string=str(m)
+# # print(string)
+# length=len(string)
+# # print(length)
+# last=string(len-1)
+# print(last)
+
+
