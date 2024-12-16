33de9c50bba6419c41f0f4a2d800728275daf95d
dearguru07 <pangalaguruyadav101@gmail.com>
2024-12-16 17:25:25 +0530

    jj

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-"use Strict";
-x=20
-console.log(x)
+// "use Strict";
+// x=20
+// console.log(x)
+
+
+const all=[1,5,'guru','charan','75',44,75,68]
+const strs=[]
+const values=[]
+
+const res=all.forEach((x)=>{
+    if(typeof(x)==='string'){
+        strs.push(x)
+    }
+    else{
+        values.push(x)
+    }
+
+})
+console.log(strs)
+console.log(values)
--- app.py
+++ app.py
@@ -38,4 +38,3 @@ Prime(n)
 def Fibbi(n):
     n1=0
     n2=1
-    n1+n2=n3
