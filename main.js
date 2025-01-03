4aa4b79473c4bc7bfa3082776a34efd67a5cfcd1
dearguru07 <pangalaguruyadav101@gmail.com>
2025-01-03 18:41:45 +0530

    kl

--- main.js
+++ main.js
@@ -6,8 +6,6 @@
 // c()
 // console.log(a)
 
-
-
 // function fetchData(callback) {
 //   setTimeout(() => {
 //     const data = { name: "John", age: 30 };
@@ -16,25 +14,56 @@
 // }
 
 // fetchData((data) => {
-//   console.log(data); 
+//   console.log(data);
 // });
 
-
 function fetchData() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const data = { name: 'John', age: 30 };
-        resolve(data);
-      }, 1000);
-    });
-  }
-  
-  fetchData().then((data) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = { name: "John", age: 30 };
+      resolve(data);
+    }, 1000);
+  });
+}
+
+fetchData()
+  .then((data) => {
     console.log(data); // { name: 'John', age: 30 }
-  }).catch((error) => {
+  })
+  .catch((error) => {
     console.error(error);
   });
 
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = { name: "John", age: 30 };
+      resolve(data);
+    }, 1000);
+  });
+}
 
-console.log('Hello world...')  
-console.log('Hello world...')
+fetchData()
+  .then((data) => {
+    console.log(data); // { name: 'John', age: 30 }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = { name: "John", age: 30 };
+      resolve(data);
+    }, 1000);
+  });
+}
+
+fetchData()
+  .then((data) => {
+    console.log(data); // { name: 'John', age: 30 }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
