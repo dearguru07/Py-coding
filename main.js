0f85203b4d4fe777f15a759fcae1fc2149a02901
dearguru07 <pangalaguruyadav101@gmail.com>
2025-01-17 13:44:55 +0530

    kki

--- main.js
+++ main.js
@@ -124,3 +124,22 @@ function c(){
 }
 c()
 
+function c(){
+    for(let i=1;i<=5;i++){
+        setTimeout(()=>{
+            console.log(i)
+        },i*1000)
+    }
+}
+c()
+
+function c(){
+    for(let i=1;i<=5;i++){
+        setTimeout(()=>{
+            console.log(i)
+        },i*1000)
+    }
+}
+c()
+
+
